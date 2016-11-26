275fcae5e72bcde74cd00aebd7f337870581e914
Mattias Gunnarsson <smgunnarsson@gmail.com>
2016-11-26 23:22:32 +0100

    Solve problem: carrots

---
+++ carrots/carrots.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+int main(int, char**)
+{
+	int ignore, answer;
+	cin >> ignore >> answer;
+	cout << answer << endl;
+	return 0;
+}
