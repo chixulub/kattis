5ad4b2e217055f19e43e8a48da70618d7179f78b
Mattias Gunnarsson <smgunnarsson@gmail.com>
2016-11-27 00:15:15 +0100

    Solve problem: bijele

---
+++ bijele/bijele.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+int main(int, char**)
+{
+	int n;
+	cin >> n;
+	cout << (1-n) << " ";
+	cin >> n;
+	cout << (1-n) << " ";
+	cin >> n;
+	cout << (2-n) << " ";
+	cin >> n;
+	cout << (2-n) << " ";
+	cin >> n;
+	cout << (2-n) << " ";
+	cin >> n;
+	cout << (8-n) << endl;
+	return 0;
+}
