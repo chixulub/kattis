f70e213251f4d5ea503be999fcbfbfdf34ff50f6
Mattias Gunnarsson <smgunnarsson@gmail.com>
2017-11-20 20:37:59 +0100

    Update problem: supercomputer
    Now uses lib:PrefixTree

--- supercomputer/supercomputer.cpp
+++ supercomputer/supercomputer.cpp
@@ -5,66 +5,91 @@
 #pragma GCC diagnostic ignored "-Wunused-result"
 using namespace std;
 
-const int N = 1 << 20;
-int32_t mem[N*2] = {0};
-
-inline int prefix(uint32_t pos)
+template<typename T = int32_t>
+struct PrefixTree
 {
-    int32_t  acc = 0;
-    uint32_t from = pos+1;
-    pos /= 2;
-    while (pos)
-    {
-        if (from&1)
-        {
-            acc += mem[from];
-        }
+	std::vector<T> m_mem;
+	uint32_t m_N;
 
-        from = pos+1;
-        pos /= 2;
-    }
+	PrefixTree(uint32_t size)
+	: m_mem(minPowerOfTwo(size)*2)
+	, m_N(minPowerOfTwo(size))
+	{
+	}
 
-    return -acc;
-}
+	void put(int pos, T newValue)
+	{
+		pos += m_N;
+		T diff = newValue - m_mem[pos];
+
+		while (pos)
+		{
+			m_mem[pos] += diff;
+			pos /= 2;
+		}
+	}
+
+	T value(uint32_t pos) const
+	{
+		return m_mem[pos + m_N];
+	}
+
+	T prefix(int pos) const
+	{
+		pos += m_N;
+		T surplus = 0;
+		uint32_t from = pos+1;
+		pos /= 2;
+		while (pos)
+		{
+			if (from&1)
+			{
+				surplus += m_mem[from];
+			}
+
+			from = pos+1;
+			pos /= 2;
+		}
+
+	    return m_mem[0] - surplus;
+	}
+
+private:
+	inline uint32_t minPowerOfTwo(uint32_t i) const
+	{
+		uint32_t n = 1;
+		while (n < i)
+		{
+			n*=2;
+		}
+
+		return n;
+	}
+};
     
-
 int main(int, char**)
 {
-    int K;
+    int n, K;
     char op;
 	uint32_t a, b;
-    scanf("%d %d", &K, &K);
+    scanf("%d %d", &n, &K);
+
+	auto* tree = new PrefixTree<int32_t>(n);
 
     while (K--)
     {
         scanf("\n%c %u", &op, &a);
-		a+=N;
         if (op == 'C')
         {
             scanf(" %u", &b);
-            printf("%d\n", prefix(b+N) - prefix(a-1));
+            printf("%d\n", tree->prefix(b) - tree->prefix(a-1));
         }
 		else
         {
-			if (mem[a])
-			{
-				while (a)
-				{
-					--mem[a];
-					a /= 2;
-				}
-			}
-			else
-			{
-				while (a)
-				{
-					++mem[a];
-					a /= 2;
-				}
-			}
+			int32_t newValue = 1 - tree->value(a);
+			tree->put(a, newValue);
         }
     }
 
     return 0;
 }
-
